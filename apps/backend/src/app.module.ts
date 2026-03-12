90e2c60ff55658f445fbf2cbb8fd4852f82a5476
Avaneesh Prajapti <prajapatiavaneesh1998@gmail.com>
2026-03-12 21:31:31 +0530

    added swagger

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
@@ -12,6 +13,9 @@ import { SupabaseModule } from './supabase/supabase.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     SupabaseModule,
     AuthModule,
     TeamsModule,
